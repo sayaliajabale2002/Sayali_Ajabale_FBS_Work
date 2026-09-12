92d9d9ffa0f66c95b7a4228a8d37504c630451dd
Sayali Ajabale <ajabalesayali30@gmail.com>
2026-09-13 01:10:26 +0530

    Refactor sumofFactorial to use recursion

--- assighnment/9 Recursion/q1.py
+++ assighnment/9 Recursion/q1.py
@@ -9,17 +9,27 @@
 #     i += 1
 # print(sum)
 
-def sumofFactorial(n,sum=0,fact = 1,count=1):
-    if(n <=0):
-        # print("0")
+# def sumofFactorial(n,sum=0,fact = 1,count=1):
+#     if(n <=0):
+#         # print("0")
+#         return 0
+#     if(n+1==count):
+#         # print(f'sum is {sum}')
+#         return sum
+#     fact *= count
+#     sum += fact
+#     count += 1
+#     return sumofFactorial(n,sum,fact,count)
+
+def factorial(num):
+    if num<=1:
+        return 1
+    return num*factorial(num-1)
+
+def sumofFactorial(n):
+    if(n<=0):
         return 0
-    if(n+1==count):
-        # print(f'sum is {sum}')
-        return sum
-    fact *= count
-    sum += fact
-    count += 1
-    return sumofFactorial(n,sum,fact,count)
+    return factorial(n)+sumofFactorial(n-1)
 
 n = int(input('enter number of terms: '))
 sum = sumofFactorial(n)
